6f84b1177504f296bd74e74d989fc1b6b84c351b
ocelotxd <ocelotokot@gmail.com>
2023-02-08 16:51:17 +0100

    banasuper

--- src/main/java/net/mcreator/jamalmod/procedures/BananaRightclickedProcedure.java
+++ src/main/java/net/mcreator/jamalmod/procedures/BananaRightclickedProcedure.java
@@ -1,5 +1,8 @@
 package net.mcreator.jamalmod.procedures;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.projectile.SpectralArrow;
@@ -7,27 +10,55 @@ import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 
 public class BananaRightclickedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		for (int index0 = 0; index0 < (int) (50); index0++) {
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getArrow(Level level, Entity shooter, float damage, int knockback, byte piercing) {
-						AbstractArrow entityToSpawn = new SpectralArrow(EntityType.SPECTRAL_ARROW, level);
-						entityToSpawn.setOwner(shooter);
-						entityToSpawn.setBaseDamage(damage);
-						entityToSpawn.setKnockback(knockback);
-						entityToSpawn.setPierceLevel(piercing);
-						return entityToSpawn;
+		if ((world.getBlockState(new BlockPos(x, y + 20, z))).getBlock() == Blocks.AIR && (world.getBlockState(new BlockPos(x, y + 22, z))).getBlock() == Blocks.AIR && (world.getBlockState(new BlockPos(x, y + 19, z))).getBlock() == Blocks.AIR
+				&& (world.getBlockState(new BlockPos(x, y + 21, z))).getBlock() == Blocks.AIR) {
+			for (int index0 = 0; index0 < (int) (500); index0++) {
+				{
+					Entity _ent = entity;
+					_ent.teleportTo(x, (y + 20), z);
+					if (_ent instanceof ServerPlayer _serverPlayer)
+						_serverPlayer.connection.teleport(x, (y + 20), z, _ent.getYRot(), _ent.getXRot());
+				}
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.note_block.bit")), SoundSource.NEUTRAL, 1, 1);
+					} else {
+						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.note_block.bit")), SoundSource.NEUTRAL, 1, 1, false);
 					}
-				}.getArrow(projectileLevel, entity, 1, 0, (byte) 5);
-				_entityToSpawn.setPos(x, y, z);
-				_entityToSpawn.shoot((entity.getLookAngle().x), (entity.getLookAngle().y), (entity.getLookAngle().z), 2, 3);
-				projectileLevel.addFreshEntity(_entityToSpawn);
+				}
+				if (world instanceof ServerLevel projectileLevel) {
+					Projectile _entityToSpawn = new Object() {
+						public Projectile getArrow(Level level, Entity shooter, float damage, int knockback, byte piercing) {
+							AbstractArrow entityToSpawn = new SpectralArrow(EntityType.SPECTRAL_ARROW, level);
+							entityToSpawn.setOwner(shooter);
+							entityToSpawn.setBaseDamage(damage);
+							entityToSpawn.setKnockback(knockback);
+							entityToSpawn.setPierceLevel(piercing);
+							return entityToSpawn;
+						}
+					}.getArrow(projectileLevel, entity, 1, 0, (byte) 5);
+					_entityToSpawn.setPos(x, y, z);
+					_entityToSpawn.shoot((entity.getLookAngle().x), (entity.getLookAngle().y), (entity.getLookAngle().z), (float) 0.5, 100);
+					projectileLevel.addFreshEntity(_entityToSpawn);
+				}
+			}
+		} else {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, new BlockPos(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.villager.no")), SoundSource.NEUTRAL, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.villager.no")), SoundSource.NEUTRAL, 1, 1, false);
+				}
 			}
 		}
 	}
