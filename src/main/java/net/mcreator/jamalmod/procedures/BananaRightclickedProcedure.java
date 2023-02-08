1bc080b8a49c155a9353530db2b7ac2d546abcf1
ocelotxd <ocelotokot@gmail.com>
2023-02-08 16:26:38 +0100

    supa dupa banana

--- src/main/java/net/mcreator/jamalmod/procedures/BananaRightclickedProcedure.java
+++ src/main/java/net/mcreator/jamalmod/procedures/BananaRightclickedProcedure.java
@@ -2,33 +2,33 @@ package net.mcreator.jamalmod.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.projectile.ThrownEgg;
+import net.minecraft.world.entity.projectile.SpectralArrow;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
-
-import net.mcreator.jamalmod.JamalmodMod;
 
 public class BananaRightclickedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		for (int index0 = 0; index0 < (int) (40); index0++) {
-			JamalmodMod.queueServerWork(1, () -> {
-				if (world instanceof ServerLevel projectileLevel) {
-					Projectile _entityToSpawn = new Object() {
-						public Projectile getProjectile(Level level, Entity shooter) {
-							Projectile entityToSpawn = new ThrownEgg(EntityType.EGG, level);
-							entityToSpawn.setOwner(shooter);
-							return entityToSpawn;
-						}
-					}.getProjectile(projectileLevel, entity);
-					_entityToSpawn.setPos(x, y, z);
-					_entityToSpawn.shoot(1, 1, 1, 1, 0);
-					projectileLevel.addFreshEntity(_entityToSpawn);
-				}
-			});
+		for (int index0 = 0; index0 < (int) (50); index0++) {
+			if (world instanceof ServerLevel projectileLevel) {
+				Projectile _entityToSpawn = new Object() {
+					public Projectile getArrow(Level level, Entity shooter, float damage, int knockback, byte piercing) {
+						AbstractArrow entityToSpawn = new SpectralArrow(EntityType.SPECTRAL_ARROW, level);
+						entityToSpawn.setOwner(shooter);
+						entityToSpawn.setBaseDamage(damage);
+						entityToSpawn.setKnockback(knockback);
+						entityToSpawn.setPierceLevel(piercing);
+						return entityToSpawn;
+					}
+				}.getArrow(projectileLevel, entity, 1, 0, (byte) 5);
+				_entityToSpawn.setPos(x, y, z);
+				_entityToSpawn.shoot((entity.getLookAngle().x), (entity.getLookAngle().y), (entity.getLookAngle().z), 2, 3);
+				projectileLevel.addFreshEntity(_entityToSpawn);
+			}
 		}
 	}
 }
