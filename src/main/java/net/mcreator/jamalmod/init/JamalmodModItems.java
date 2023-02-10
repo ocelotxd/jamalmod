ae61c92a2224a44de23a37cdb0eff7aa9386cd28
ppp <ppp@DESKTOP-NMM6RCN>
2023-02-10 18:34:51 +0100

    MCreator Merge commit '05144b753c7d6026b167a5f4b0e9d9a8e012a3a6' into 9df2d1116bc99a19cff415ec918ec412a0c34afc

--- src/main/java/net/mcreator/jamalmod/init/JamalmodModItems.java
+++ src/main/java/net/mcreator/jamalmod/init/JamalmodModItems.java
@@ -7,16 +7,13 @@ package net.mcreator.jamalmod.init;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.common.ForgeSpawnEggItem;
 
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
 
 import net.mcreator.jamalmod.item.BananaItem;
 import net.mcreator.jamalmod.JamalmodMod;
 
 public class JamalmodModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, JamalmodMod.MODID);
-	public static final RegistryObject<Item> TURRET_SPAWN_EGG = REGISTRY.register("turret_spawn_egg", () -> new ForgeSpawnEggItem(JamalmodModEntities.TURRET, -1, -1, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 	public static final RegistryObject<Item> BANANA = REGISTRY.register("banana", () -> new BananaItem());
 }
